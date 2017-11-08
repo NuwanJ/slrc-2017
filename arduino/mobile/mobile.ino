9233493824426f8712a8d5a99b0deaeba70acc2b
NuwanJ <nuwanjaliyagoda@gmail.com>
2017-11-08 21:45:33 +0530

    begin

---
+++ arduino/mobile/mobile.ino
@@ -0,0 +1,20 @@
+
+#include <Arduino.h>
+//#include <EEPROM.h>
+//#include <Wire.h>
+//#include <SoftwareSerial.h>
+#include "define.h"
+
+//SoftwareSerial mySerial(3, 2); // RX, TX
+
+volatile int mode = BEGIN;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void test() {
+
+
+}
+

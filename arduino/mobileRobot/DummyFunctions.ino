f5646423cbfdba741ae74c9a06e26efbfcd7d630
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2018-01-02 06:20:19 +0530

    Maze following 02/01/2017 1

---
+++ arduino/mobileRobot/DummyFunctions.ino
@@ -0,0 +1,22 @@
+void turnCW(int deg) {
+  //This function should turn the robot CW by deg
+  //Should work for both positive and negative values of deg
+}
+
+
+int sum(int ar[]) {
+  //This should work for both arrays and pointers
+  int ssum = 0;
+  for (int x = 0; x < ar.length; x++)ssum += ar[x];
+  return ssum;
+}
+
+void goForward() {
+  //The robot should go one step forward. This is to ignore the turns to left while following lines
+}
+
+
+void goBack() {
+  //The robot should go back just one step
+}
+

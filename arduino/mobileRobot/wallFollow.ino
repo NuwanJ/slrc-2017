561923fc14e5675d67458edba82af96f4c50e2ef
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-01-03 14:45:17 +0530

    corrections

--- arduino/mobileRobot/wallFollow.ino
+++ arduino/mobileRobot/wallFollow.ino
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------
   Functions
----------------------------------------------------------------------------*/
+  ---------------------------------------------------------------------------*/
 
 // Contributor :
 // Last Update : 27/11/2017
@@ -9,57 +9,57 @@
 
 
 
-----------------------------------------------*/
+  ----------------------------------------------*/
 
 
-int fitToLeft(int * diff, int baseSpeed){
-  int left,right;
-  if (diff[3]<0 && diff[2]>0){ // front left is too close to wall. going forward may hit the wall. therefore spin on the spot
-    left=-50;
-    right=50;
-    motorWrite(-1*left,-1*right);
-    left=30;right=30; // go a forward a little
-  }else if (diff[2]<0 && diff[3]>0){ // back left is too close. if go forward, front may cross the line. spin!
-    left=50;
-    right=-50;
-    motorWrite(-1*left,-1*right);
-    left=30;right=30; // go a forward a little
-  }else if (diff[2]<0 && diff[3]<0){ // TODO: left side is too close!! what to do???
+int fitToLeft(int * diff, int baseSpeed) {
+  int left, right;
+  if (diff[3] < 0 && diff[2] > 0) { // front left is too close to wall. going forward may hit the wall. therefore spin on the spot
+    left = -50;
+    right = 50;
+    motorWrite(1 * left, 1 * right);
+    left = 30; right = 30; // go a forward a little
+  } else if (diff[2] < 0 && diff[3] > 0) { // back left is too close. if go forward, front may cross the line. spin!
+    left = 50;
+    right = -50;
+    motorWrite(1 * left, 1 * right);
+    left = 30; right = 30; // go a forward a little
+  } else if (diff[2] < 0 && diff[3] < 0) { // TODO: left side is too close!! what to do???
     left = 0;
     right = 0;
-  }else if (diff[2] > diff[3]){ // oriented towards the wall.
+  } else if (diff[2] > diff[3]) { // oriented towards the wall.
     left = baseSpeed;
     right = baseSpeed;
-  }else{ // oriented outwards to the left wall
-    int val = diff[3]*20 + diff[2]*10; // more weight to the front sensor
+  } else { // oriented outwards to the left wall
+    int val = diff[3] * 20 + diff[2] * 10; // more weight to the front sensor
     left = baseSpeed - val;
     right = baseSpeed + val;
   }
-  
+
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(diff[3]);
   lcd.setCursor(0, 1);
   lcd.print(diff[2]);
-  
+
   lcd.setCursor(0, 0);
   lcd.print(left);
   lcd.setCursor(10, 1);
   lcd.print(right);
-  
-  motorWrite(-1*left,-1*right);
+
+  motorWrite(1 * left, 1 * right);
 }
 
 
-int fitToRight(int * diff, int baseSpeed){
-  
+int fitToRight(int * diff, int baseSpeed) {
+
 }
 
-int wallFollow(int baseSpeed){
+int wallFollow(int baseSpeed) {
   int thresh = 5;
   int diff[4];
-  
-  for (int i=0;i<4;i++){
+
+  for (int i = 0; i < 4; i++) {
     dist[i] = readSonar(i);
     diff[i] = dist[i] - thresh;
   }
@@ -68,11 +68,11 @@ int wallFollow(int baseSpeed){
   cost[0] = abs(diff[2]) + abs(diff[3]);
   cost[1] = abs(diff[0]) + abs(diff[1]);
 
-  if (cost[0] < cost[1]){ // easy to fit the robot to the left wall
+  if (cost[0] < cost[1]) { // easy to fit the robot to the left wall
     fitToLeft(diff, baseSpeed);
-  }else{ // easy to fit to right wall
+  } else { // easy to fit to right wall
     fitToRight(diff, baseSpeed);
   }
-  
-  
+
+
 }

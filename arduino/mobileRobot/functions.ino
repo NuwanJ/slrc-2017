f136ac01a399a0bb13e5475183a574af9ce75afa
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2018-01-04 11:09:29 +0530

    PID complete, Maze follow with rejection complete

--- arduino/mobileRobot/functions.ino
+++ arduino/mobileRobot/functions.ino
@@ -217,17 +217,29 @@ void backToPath() {
   motorStop();
   }*/
 
-void util_readSensorAndUpdateRejectListCW(int* sensor_vals, boolean[] reject) {
-  readIRSensors(sensor_vals);
-  if (sensor_vals[5] == 0) reject[5] = false;
-  for (int x = 4; x > -1; x--) {
-    if ( (!reject[x + 1]) and (sensor_vals[x] == 0)) {
-      reject[x] = false;
+void util_readSensorAndUpdateRejectListCW(int* sensor_vals, boolean reject[], int dir) {
+  if (dir == CW) {
+    readIRSensors(sensor_vals);
+    if (sensor_vals[5] == 0) reject[5] = false;
+    for (int x = 4; x > -1; x--) {
+      if ( (!reject[x + 1]) and (sensor_vals[x] == 0)) {
+        reject[x] = false;
+      }
+    }
+  }
+  else {
+    readIRSensors(sensor_vals);
+    if (sensor_vals[0] == 0) reject[0] = false;
+    for (int x = 1; x < 6; x++) {
+      if ( (!reject[x - 1]) and (sensor_vals[x] == 0)) {
+        reject[x] = false;
+      }
     }
   }
 }
 
-int util_nonRejectSum(int* array, boolean[] reject) {
+
+int util_nonRejectSum(int* array, boolean reject[]) {
   int summ = 0;
   for (int x = 0; x < 6; x++)if (!reject[x])summ += array[x];
   return summ;
@@ -237,46 +249,58 @@ int util_nonRejectSum(int* array, boolean[] reject) {
 void alignToPath2(int dir) {
   /*
      New version, uses rejection techniques
+     Use this to replace 
+            void alignToPath(int dir)
   */
 
-  if (dir == CW) {
-    boolean reject = {true, true, true, true, true, true};
 
-    util_readSensorAndUpdateRejectListCW(sensor_values,reject);
-    
-
-
-
-
-    readIRSensors(sensor_values);
-    //Turn until the sensor panel leaves the first line.
-    while (sensor_values[5] != 0) {
-      motorWrite(150, 0);
-      delay(20);
-      motorWrite(0, 0);
-      readIRSensors(sensor_values);
-    }
-    //Turn until the sensor panel finds the new line.
-    while (sensor_values[5] == 0) {
-      motorWrite(150, 0);
-      delay(20);
-      motorWrite(0, 0);
-      readIRSensors(sensor_values);
-    }
-
-    //Take the sensor panel a little more into the new line
-    motorWrite(150, 0);
-    delay(100); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Tune-able parameter
-    motorWrite(0, 0);
-
-    //Now align the robot to the line
-    for (int i = 0; i < 5; i++) {
-      int r = readIRSensors(sensor_values) - 25;
-      motorWrite(r, -1 * r);
-      delay((6 - i) * 10);
-    }
-    //The sensor panel is alligned to the line, move forward
+  int motorSpeeds[] = {150, 0};
+  int cornerSensor = 5;
+  if (dir == CCW) {
+    motorSpeeds[0] = 0;
+    motorSpeeds[1] = 150;
+    cornerSensor = 0;
   }
+
+  boolean reject[] = {true, true, true, true, true, true};
+
+  //Turn until the sensor panel leaves the first line.
+  while (!reject[cornerSensor] & (sensor_values[cornerSensor] == 1)) {
+    motorWrite(motorSpeeds[0], motorSpeeds[1]);
+    delay(20);
+    motorWrite(0, 0);
+    readIRSensors(sensor_values);
+    util_readSensorAndUpdateRejectListCW(sensor_values, reject, dir);
+  }
+
+  //Turn until the sensor panel finds the new line.
+  while (sensor_values[cornerSensor] == 0) {
+    motorWrite(motorSpeeds[0], motorSpeeds[1]);
+    delay(20);
+    motorWrite(0, 0);
+    util_readSensorAndUpdateRejectListCW(sensor_values, reject, dir);
+  }
+
+  //Take the sensor panel a little more into the new line
+  motorWrite(motorSpeeds[0], motorSpeeds[1]);
+  delay(100); //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Tune-able parameter
+  motorWrite(0, 0);
+
+  //Now align the robot to the line
+  for (int i = 0; i < 5; i++) {
+    util_readSensorAndUpdateRejectListCW(sensor_values, reject, dir);
+    float s = 0.0f;
+    for (int x = 0; x < 6; x++) {
+      if (!reject[x]) {
+        s += (5 * (x - 2.5) * sensor_values[x]);
+      }
+    }
+    s *= 1.0; //<<<<<<<<<<<<<<<<<<<<<< A TUNE-ABLE PARAMETER
+    motorWrite(s * -1, s);
+    delay((6 - i) * 10);
+  }
+  //The sensor panel is alligned to the line, move forward
+
 }
 void alignToPath(int dir) {
   /*
--- arduino/mobileRobot/mazeFollow.ino
+++ arduino/mobileRobot/mazeFollow.ino
@@ -23,28 +23,25 @@ void mazeFollow() {
   Serial.println(irLineString);
 
   if (sensor_values[5] > 0) {
-    motorWrite(150, 150);
-    delay(100);
-    turnCW(-90);
-    lcdWrite(0, "Maze:turn-90");
+    lcdWrite(0, "Maze:trnCW90");
+    alignToPath2(CW);
   }
   else if (arsum(sensor_values) == 0) {
     goBack();
     if (lastReading <= -15) {
+      lcdWrite(0, "Maze:turn90");
       motorWrite(150, 150);
       delay(100);
       turnCW(90);
-      lcdWrite(0, "Maze:turn90");
     }
     else {
-      //lcdWrite(0, "Maze:turnBck");
-      //turnCW(180);
+      lcdWrite(0, "Maze:turnBck");
+      alignToPath2(CW);
     }
   }
   else if (sensor_values[0] > 0) {
-    lcdWrite(0, "Maze:goFwd");
-    //    goForward();
-    lineFollow(); //This is wrong, remove this and uncomment goForward()
+    lcdWrite(0, "Maze:GoFwd");
+    goForward();
     delay(100);
   }
   else {

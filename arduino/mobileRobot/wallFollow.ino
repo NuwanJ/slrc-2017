915cba95ca8865a33995b0496a058dcc705fd9fa
Gihan Chanaka Jayatilaka <gihanchanaka@gmail.com>
2018-01-07 14:04:17 +0530

    Fixed wall follow Harshana-Gihan mixings.
    Harshana wallFollow.ino
    Gihan wallFollow2.ino

--- arduino/mobileRobot/wallFollow.ino
+++ arduino/mobileRobot/wallFollow.ino
@@ -6,6 +6,7 @@
 // Last Update : 6/1/2018
 
 /* Remarks ------------------------------------
+ *  These are the functions written by Hashana, dont mix with what Gihan has written on the file wallFollow2.ino
   ----------------------------------------------*/
 
 int fit(double front, double back, int flag, int baseSpeed) {
@@ -32,7 +33,7 @@ int fit(double front, double back, int flag, int baseSpeed) {
     if (abs(front - back) > 5) {
       beep(2);
       front = 0; back = 0;
-      return;u
+      return ; //<<<<< @Harshana: There is a mistake on ur https://github.com/harshana95/slrc-2017/blob/master/arduino/mobileRobot/wallFollow.ino
     }
     double P = (front - back) * flag * 30;
     double I = 0;
@@ -102,4 +103,78 @@ int wallFollow(int baseSpeed) {
       //fit(dist[3], dist[2], 1, baseSpeed);
     }
   
+}
+
+
+
+/*
+int wallFollow(int baseSpeed) {
+  int thresh = 10;
+  int diff[4];
+
+  for (int i = 0; i < 4; i++) {
+    dist[i] = readSonar(i);
+    if (dist[i] > 200) {
+      motorWrite(0, 0);
+      return -1;
+    }
+    diff[i] = thresh - dist[i];
+  }
+  followLeft(dist[0], dist[1]);
+}
+*/
+int fitpid(int front, int back, int flag, int baseSpeed) {
+
+  return 0;
+}
+
+int fit(int front, int back, int flag, int baseSpeed) {
+  int left, right;
+  if (front > 0 && back < 0) { // front is too close to wall. going forward may hit the wall. therefore spin
+    motorWrite(75 - 75 * flag, 75 + 75 * flag);
+    delay(50);
+    motorWrite(0, 0);
+
+    left = 150; right = 150; // go forward a little
+  } else if (back > 0 && front < 0) { // back is too close. if go forward, front may cross the line. spin!
+    motorWrite(75 + 75 * flag, 75 - 75 * flag);
+    delay(50);
+    motorWrite(0, 0);
+
+    left = 150; right = 150; // go forward a little
+  } else if (front > 0 && back > 0) { // TODO: side is too close!! what to do???
+    motorWrite(75 - 75 * flag, 75 + 75 * flag);
+    delay(50);
+    motorWrite(0, 0);
+    left = 0;
+    right = 0;
+
+  } else if (back == front) { // oriented towards the wall.
+    left = baseSpeed;
+    right = baseSpeed;
+  } else if (back < front) {
+    left = 150;
+    right = 0;
+  } else { // oriented outwards to the wall
+    int val = -1 * (front * 50 + back * 10); // more weight to the front sensor
+    left = baseSpeed - val * flag;
+    right = baseSpeed + val * flag;
+
+    left = 0;
+    right = 150;
+  }
+  motorWrite(left, right);
+  delay(50);
+  motorWrite(0, 0);
+
+}
+
+int fitToLeft(int * diff, int baseSpeed) {
+  fit(diff[0], diff[1], -1, baseSpeed);
+  //fitpid(diff[0], diff[1], -1, baseSpeed);
+}
+
+
+int fitToRight(int * diff, int baseSpeed) {
+  fit(diff[3], diff[2], 1, baseSpeed);
 }

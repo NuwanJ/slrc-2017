6c2b021b971504a34a242fc714ddac4da3ef3e5e
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-01-10 18:01:42 +0530

    Communication Updated

--- arduino/mobileRobot/serial.ino
+++ arduino/mobileRobot/serial.ino
@@ -37,33 +37,44 @@ void serialEvent() {
   }
 }
 
-void serialEvent3() {
+void SerialEvent3() {
+  //This happens if the stationary bot says it finished shooting
 
   while (Serial3.available()) {
-
     digitalWrite(LED_RED, HIGH);
 
     int r = Serial3.read();
     char inChar = (char)r;
 
-#ifdef LCD_SERIAL_DEBUG
-    // Need to implement
-    Serial.println(inChar);
-    lcdWrite(0, "");
-#endif
-
-    //-------------------------------------------------------------------------------------------------------------- s -> stopvnlv ,nvxcbvxcvmxcvmcxv
-    if (inChar == 's') {
-      mode = BEGIN;
-      motorWrite(0, 0);
+    if (inChar == 'd') {
+      mode = RETURN_TO_MAZE;
     }
-
-    //-------------------------------------------------------------------------------------------------------------- t -> test
-    else if (inChar == 't') {
-
-    }
-
-    digitalWrite(LED_RED, LOW);
   }
+  delay(50);
+  digitalWrite(LED_RED, LOW);
 }
 
+
+
+
+void shoot(int color) {
+
+  digitalWrite(LED_RED, HIGH);
+
+  switch (color) {
+    case COLOR_RED:
+      Serial3.print('r');
+      break;
+    case COLOR_GREEN:
+      Serial3.print('g');
+      break;
+    case COLOR_BLUE:
+      Serial3.print('b');
+      break;
+  }
+  timeOfShootCommand = millis();
+  mode = WAIT_UNTIL_FEEDBACK;
+
+  delay(150);
+  digitalWrite(LED_RED, LOW);
+}
